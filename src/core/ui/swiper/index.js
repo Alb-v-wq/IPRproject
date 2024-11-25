e54e6e3da4f6c69c252107c5b64aae2fec0ac9a1
Alb-v-wq <malyarovay@gmail.com>
2024-11-25 13:23:27 +0300

    commit 1 feat_2

--- src/core/ui/swiper/index.js
+++ src/core/ui/swiper/index.js
@@ -7,11 +7,11 @@
         }
 
         const sliderCategory = element.querySelector('.s-catalog__swiper') || element.closest('.s-catalog__swiper');
-        //const reviewSlider = element.querySelector('СЮДА ВСТАВИШЬ СВОЙ КЛАСС') || element.closest('СЮДА ВСТАВИШЬ СВОЙ КЛАСС');
+        // const reviewSlider = element.querySelector('.s-reviews__swiper') || element.closest('.s-reviews__swiper');
 
 
         new Swiper(sliderCategory, {
-            slidesPerView: 4,
+            slidesPerView: 3,
             spaceBetween: 20,
             navigation: {
                 nextEl: '.swiper-button-next',
@@ -26,32 +26,32 @@
                     slidesPerView: 2,
                 },
                 1279: {
-                    slidesPerView: 4,
+                    slidesPerView: 3,
                 },
             }
         });
 
 
-        // new Swiper(reviewSlider, {
-        //     slidesPerView: 4,
-        //     spaceBetween: 20,
-        //     navigation: {
-        //         nextEl: '.swiper-button-next',
-        //         prevEl: '.swiper-button-prev',
-        //     },
-        //     breakpoints: {
-        //         1: {
-        //             slidesPerView: 2,
-        //             spaceBetween: 12,
-        //         },
-        //         767: {
-        //             slidesPerView: 2,
-        //         },
-        //         1279: {
-        //             slidesPerView: 4,
-        //         },
-        //     }
-        // });
+    //     new Swiper(reviewSlider, {
+    //         slidesPerView: 3,
+    //         spaceBetween: 20,
+    //         navigation: {
+    //             nextEl: '.swiper-button-next',
+    //             prevEl: '.swiper-button-prev',
+    //         },
+    //         breakpoints: {
+    //             1: {
+    //                 slidesPerView: 2,
+    //                 spaceBetween: 12,
+    //             },
+    //             767: {
+    //                 slidesPerView: 2,
+    //             },
+    //             1279: {
+    //                 slidesPerView: 3,
+    //             },
+    //         }
+    //     });
     };
 
     const mount = () => {

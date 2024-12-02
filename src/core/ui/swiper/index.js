2fc31870ea43b13552b35d187555af64ff0801f0
Alb-v-wq <malyarovay@gmail.com>
2024-12-02 16:16:15 +0300

    commit 7 (final) feat_2

--- src/core/ui/swiper/index.js
+++ src/core/ui/swiper/index.js
@@ -7,10 +7,32 @@
         }
 
         const sliderCategory = element.querySelector('.s-catalog__swiper') || element.closest('.s-catalog__swiper');
-        // const reviewSlider = element.querySelector('.s-reviews__swiper') || element.closest('.s-reviews__swiper');
+        const reviewsSlider = element.querySelector('.s-reviews__swiper') || element.closest('.s-reviews__swiper');
 
 
         new Swiper(sliderCategory, {
+            slidesPerView: 3,
+            spaceBetween: 20,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                1: {
+                    slidesPerView: 1,
+                    spaceBetween: 12,
+                },
+                767: {
+                    slidesPerView: 1,
+                },
+                1279: {
+                    slidesPerView: 3,
+                },
+            }
+        });
+
+
+        new Swiper(reviewsSlider, {
             slidesPerView: 3,
             spaceBetween: 20,
             navigation: {
@@ -30,28 +52,6 @@
                 },
             }
         });
-
-
-        // new Swiper(reviewSlider, {
-        //     slidesPerView: 3,
-        //     spaceBetween: 20,
-        //     navigation: {
-        //         nextEl: '.swiper-button-next',
-        //         prevEl: '.swiper-button-prev',
-        //     },
-        //     breakpoints: {
-        //         1: {
-        //             slidesPerView: 2,
-        //             spaceBetween: 12,
-        //         },
-        //         767: {
-        //             slidesPerView: 2,
-        //         },
-        //         1279: {
-        //             slidesPerView: 3,
-        //         },
-        //     }
-        // });
     };
 
     const mount = () => {

1acdc4c1ac10d0a9b14d553aa48e14592ab55ed5
Alb-v-wq <malyarovay@gmail.com>
2024-12-03 17:01:30 +0300

    Feat 2 (#3)
    * commit 1 feat_2
    
    * commit 6 (feat 2)
    
    * commit 7 (final) feat_2
    
    * commit 8 feat_2

--- src/core/ui/swiper/index.js
+++ src/core/ui/swiper/index.js
@@ -7,11 +7,33 @@
         }
 
         const sliderCategory = element.querySelector('.s-catalog__swiper') || element.closest('.s-catalog__swiper');
-        //const reviewSlider = element.querySelector('СЮДА ВСТАВИШЬ СВОЙ КЛАСС') || element.closest('СЮДА ВСТАВИШЬ СВОЙ КЛАСС');
+        const reviewsSlider = element.querySelector('.s-reviews__swiper') || element.closest('.s-reviews__swiper');
 
 
         new Swiper(sliderCategory, {
-            slidesPerView: 4,
+            slidesPerView: 3,
+            spaceBetween: 20,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                1: {
+                    slidesPerView: 1,
+                    spaceBetween: 12,
+                },
+                767: {
+                    slidesPerView: 1,
+                },
+                1279: {
+                    slidesPerView: 3,
+                },
+            }
+        });
+
+
+        new Swiper(reviewsSlider, {
+            slidesPerView: 3,
             spaceBetween: 20,
             navigation: {
                 nextEl: '.swiper-button-next',
@@ -26,32 +48,10 @@
                     slidesPerView: 2,
                 },
                 1279: {
-                    slidesPerView: 4,
+                    slidesPerView: 3,
                 },
             }
         });
-
-
-        // new Swiper(reviewSlider, {
-        //     slidesPerView: 4,
-        //     spaceBetween: 20,
-        //     navigation: {
-        //         nextEl: '.swiper-button-next',
-        //         prevEl: '.swiper-button-prev',
-        //     },
-        //     breakpoints: {
-        //         1: {
-        //             slidesPerView: 2,
-        //             spaceBetween: 12,
-        //         },
-        //         767: {
-        //             slidesPerView: 2,
-        //         },
-        //         1279: {
-        //             slidesPerView: 4,
-        //         },
-        //     }
-        // });
     };
 
     const mount = () => {
